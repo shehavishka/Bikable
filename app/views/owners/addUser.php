275c9c48a0cbea6490e66ee19aabd94f771d9178
shehavishka <2020cs018@stu.ucsc.cmb.ac.lk>
2023-01-17 18:17:05 +0530

    add user php file added

---
+++ app/views/owners/addUser.php
@@ -0,0 +1,194 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="adduser.css">
+    <title>Document</title>
+</head>
+<body>
+    <!-- finalized side bar -->
+    <section class="sidebar">
+        <div class="viewData">
+            <div class="sidebar--logo">
+                <div class="sidebar--logo--img">
+                    <img src="images/bikableLogo/logo.PNG" alt="">
+                </div>
+            </div>
+            <div class="sidebar__detail">
+                <div class="detail detail__dashboard">
+                    Dashboard
+                </div>
+                <hr>
+                <div class="detail detail__users">
+                    <div class="detail__users--title">
+                        Users
+                    </div>
+                    <div class="detail__users--submenu">
+                        <div class="submenu users__submenu--mechanic">
+                            Mechanic
+                        </div>
+                        <div class="submenu users__submenu--riders">
+                            Riders
+                        </div>
+                        <div class="submenu users__submenu--bicycleOwners">
+                            Bicycle Owners
+                        </div>
+                        <div class="submenu users__submenu--administrators">
+                            Administrators
+                        </div>
+                    </div>
+                </div>
+                <hr>
+                <div class="detail detail__repairLog">
+                    <div class="detail__repairlog--title">
+                        Repair Log
+                    </div>
+                </div>
+                <hr>
+                <div class="detail detail__dockingAreas">
+                    <div class="detail__dockingAreas--title">
+                        Docking Areas
+                    </div>
+                </div>
+                <hr>
+                <div class="detail detail__bicycles">
+                    <div class="detail__bicycles--title">
+                        Bicycles
+                    </div>
+                </div>
+                <hr>
+                <div class="detail detail__rides">
+                    <div class="detail__rides--title">
+                        Rides
+                    </div>
+                </div>
+                <hr>
+                <div class="detail detail__reports">
+                    <div class="detail__reports--title">
+                        Reports
+                    </div>
+                    <div class="detail__reports--submenu">
+                        <div class="submenu reports__submenu--accidentReport">
+                            Accident Reports
+                        </div>
+                        <div class="submenu users__submenu--bikeComplaintReport">
+                            Bike Complaint Reports
+                        </div>
+                        <div class="submenu users__submenu--dockingAreaReport">
+                            Docking Area Reports
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+
+    <!-- In the framework right side of the web page view -->
+    <section class="data_area">
+
+        <!-- dashboard section -->
+        <section class="dashboard--header">
+            <!-- <div class="dashboard__header--title"><strong> Dashboard</strong></div>
+             -->
+            <div class="dashboard__header--search">
+                <input type="text" class="dashboard__header--searchbox" name="dashboard--searchbox" placeholder="Search">
+                <div class="dashboard__header--searchicon">
+                    <img src="images/dashboardIcons/search.png" alt="search icon" class="dashboard__icon searchicon">
+                </div>
+            </div>
+    
+            <div class="dashboard__header--helpsetting">
+                <div class="helpsetting__help">
+                    <img src="images/dashboardIcons/question.png" alt="help" class="dashboard__icon">
+                </div>
+                <div class="helpsetting__setting">
+                    <img src="images/dashboardIcons/setting.png" alt="setting" class="dashboard__icon">
+                </div>
+            </div>
+    
+            <div class="dashboard__user__detail">
+                <div class="user__address">Hello, Shehaan</div>
+                <img src="images/avatar.png" alt="dashboard profile picture" class="imgProperty">
+            </div>      
+        </section>
+
+        <!-- REAL DATA AREA -->
+
+        <!-- admin real data top -->
+        <div class="data__area--top">
+            <div class="data__area__top--title">Add User</div>
+            <div class="data_area__top--twobuttons">
+                <div class="add_user_button">
+                    <input type="button" class="btn btn_add" value="Cancel" onclick="location.href='<?php echo URLROOT;?>/owners/addAdministrator'">
+                </div>
+                <div class="delete_user_button">
+                    <input type="button" class="btn btn_delete" value="Submit" onclick="location.href='<?php echo URLROOT;?>/owners/addAdministrator'">
+                </div>
+            </div>
+
+        </div>
+
+        <div class="data__area--detail">
+
+            <div class="data__area__detail--name">
+
+                <div class="data__area__div data--firstname">
+                    <div class="data--name--lebal">First Name</div>
+                    <input type="text" class="detailbox" name="first_name" placeholder="First name" id="fName">
+                </div>
+
+                <div class="data__area__div data--lastname">
+                    <div class="data--name--lebal">Last Name</div>
+                    <input type="text" class="detailbox" name="last_name" placeholder="Last name" id="lName">
+                </div>
+
+            </div>
+
+            <div class="data__area__detail--email">
+                <div class="data--email--lebal">Email Address</div>
+                <input type="email" class="detailbox detailbox--email" name="email" placeholder="Email" id="email">
+            </div>
+
+            <div class="data__area__detail--status">
+                <div class="data--name--lebal">Status</div>
+                <div class="data__status">
+                    <div class="data__status--active">
+                        <input type="radio"><label for="active">Active</label>
+                    </div>
+                    <div class="data__status--inactive">
+                        <input type="radio"><label for="inactive">Inactive</label>
+                    </div>
+                </div>
+            </div>
+
+            <div class="data__area__detail--number">
+                <div class="data__area__detail--nic_number">
+                    <div class="data--name--lebal">NIC Number</div>
+                    <input type="text" class="detailbox" name="nic_number" placeholder="NIC Number" id="fName">
+                </div>
+                <div class="data__area__detai--contact_number">
+                    <div class="data--name--lebal">Contact Number</div>
+                    <input type="text" class="detailbox" name="contact_number" placeholder="Contact Number" id="contact_number">
+                </div>
+            </div>
+
+            <div class="data__area__detail--role">
+                <div class="data--name--lebal">Role</div>
+                <input type="text" class="detailbox_userrole detailbox" name="contact_number" placeholder="Role" id="contact_number" list="roles">
+                <datalist id="roles">
+                    <option value="Administrator">
+                    <option value="Mechanic">
+                    <option value="Rider">
+                </datalist>
+            </div>
+
+        </div>
+    </section>
+
+
+
+</body>
+</html>
