22eecc35d14d0f1f990d34cf72f4d8ab51c5a955
shehavishka <2020cs018@stu.ucsc.cmb.ac.lk>
2023-01-23 10:43:03 +0530

    side bar implemented seperate from the views

---
+++ app/views/inc/sidebar.php
@@ -0,0 +1,76 @@
+    <!-- finalized side bar -->
+    <section class="sidebar">
+        <div class="viewData">
+            <div class="sidebar--logo">
+                <div class="sidebar--logo--img">
+                    <img src="<?php echo URLROOT;?>/public/images/z_bikableLogo/logo.PNG" alt="">
+                </div>
+            </div>
+            <div class="sidebar__detail">
+                <div class="detail detail__dashboard">
+                    <div class="detail__dashboard--name" ><a href="<?php echo URLROOT ?>/owners/ownerLandPage">Dashboard</a></div>
+                </div>
+                <hr>
+                <div class="detail detail__users">
+                    <div class="detail__users--title">
+                        Users
+                    </div>
+                    <div class="detail__users--submenu">
+                        <div class="submenu users__submenu--mechanic">
+                            Mechanic
+                        </div>
+                        <div class="submenu users__submenu--riders">
+                            Riders
+                        </div>
+                        <div class="submenu users__submenu--bicycleOwners">
+                            Bicycle Owners
+                        </div>
+                        <div class="submenu users__submenu--administrators">
+                            Administrators
+                        </div>
+                    </div>
+                </div>
+                <hr>
+                <div class="detail detail__repairLog">
+                    <div class="detail__repairlog--title">
+                    <div class="detail__dashboard--name" ><a href="<?php echo URLROOT ?>/owners/addNewRepairLog">Repair Log</a></div>
+                    </div>
+                </div>
+                <hr>
+                <div class="detail detail__dockingAreas">
+                    <div class="detail__dockingAreas--title">
+                        Docking Areas
+                    </div>
+                </div>
+                <hr>
+                <div class="detail detail__bicycles">
+                    <div class="detail__bicycles--title">
+                        Bicycles
+                    </div>
+                </div>
+                <hr>
+                <div class="detail detail__rides">
+                    <div class="detail__rides--title">
+                        Rides
+                    </div>
+                </div>
+                <hr>
+                <div class="detail detail__reports">
+                    <div class="detail__reports--title">
+                        Reports
+                    </div>
+                    <div class="detail__reports--submenu">
+                        <div class="submenu reports__submenu--accidentReport">
+                            Accident Reports
+                        </div>
+                        <div class="submenu users__submenu--bikeComplaintReport">
+                            Bike Complaint Reports
+                        </div>
+                        <div class="submenu users__submenu--dockingAreaReport">
+                            Docking Area Reports
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
