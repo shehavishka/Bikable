32a5d1698f11aa6ba333c28b050212e6d69538bd
shehavishka <2020cs018@stu.ucsc.cmb.ac.lk>
2023-01-23 11:21:40 +0530

    dockingareas.php file linked with the css file

--- app/views/owners/dockingareas.php
+++ app/views/owners/dockingareas.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="dockingareas.css">
+    <link rel="stylesheet" href="<?php echo URLROOT; ?>/public/css/owners/dockingareas.css">
     <title>Docking Areas</title>
 </head>
 <body>
