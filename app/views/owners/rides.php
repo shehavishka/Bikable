e950994d6a2405025338ebe27697c9a9e677096f
shehavishka <2020cs018@stu.ucsc.cmb.ac.lk>
2023-01-25 07:52:04 +0530

    take off side bar from the rides

--- app/views/owners/rides.php
+++ app/views/owners/rides.php
@@ -4,86 +4,12 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="rides.css">
+    <link rel="stylesheet" href="<?php echo URLROOT; ?>/public/css/owners/rides.css">
     <title>Rides</title>
 </head>
 <body>
     <!-- finalized side bar -->
-    <section class="sidebar">
-        <div class="viewData">
-            <div class="sidebar--logo">
-                <div class="sidebar--logo--img">
-                    <img src="images/bikableLogo/logo.PNG" alt="">
-                </div>
-            </div>
-            <div class="sidebar__detail">
-                <div class="detail detail__dashboard">
-                    Dashboard
-                </div>
-                <hr>
-                <div class="detail detail__users">
-                    <div class="detail__users--title">
-                        Users
-                    </div>
-                    <div class="detail__users--submenu">
-                        <div class="submenu users__submenu--mechanic">
-                            Mechanic
-                        </div>
-                        <div class="submenu users__submenu--riders">
-                            Riders
-                        </div>
-                        <div class="submenu users__submenu--bicycleOwners">
-                            Bicycle Owners
-                        </div>
-                        <div class="submenu users__submenu--administrators">
-                            Administrators
-                        </div>
-                    </div>
-                </div>
-                <hr>
-                <div class="detail detail__repairLog">
-                    <div class="detail__repairlog--title">
-                        Repair Log
-                    </div>
-                </div>
-                <hr>
-                <div class="detail detail__dockingAreas">
-                    <div class="detail__dockingAreas--title">
-                        Docking Areas
-                    </div>
-                </div>
-                <hr>
-                <div class="detail detail__bicycles">
-                    <div class="detail__bicycles--title">
-                        Bicycles
-                    </div>
-                </div>
-                <hr>
-                <div class="detail detail__rides">
-                    <div class="detail__rides--title">
-                        Rides
-                    </div>
-                </div>
-                <hr>
-                <div class="detail detail__reports">
-                    <div class="detail__reports--title">
-                        Reports
-                    </div>
-                    <div class="detail__reports--submenu">
-                        <div class="submenu reports__submenu--accidentReport">
-                            Accident Reports
-                        </div>
-                        <div class="submenu users__submenu--bikeComplaintReport">
-                            Bike Complaint Reports
-                        </div>
-                        <div class="submenu users__submenu--dockingAreaReport">
-                            Docking Area Reports
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </section>
+    <?php require APPROOT . '/views/inc/sidebar.php'; ?>
 
 
     <!-- In the framework right side of the web page view -->
