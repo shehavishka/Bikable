25f88281207e71aa0265e3eaa5dd13e06078966f
shehavishka <2020cs018@stu.ucsc.cmb.ac.lk>
2023-01-27 11:20:56 +0530

    connect application to the real database

--- app/config/config.php
+++ app/config/config.php
@@ -6,6 +6,6 @@
     define('DB_HOST', 'localhost');
     define('DB_USER', 'root');
     define('DB_PASS', '');
-    define('DB_NAME', 'bikable');
+    define('DB_NAME', 'Bikable');
 
     define('URLROOT', 'http://localhost/Bikable_23.1.1');
