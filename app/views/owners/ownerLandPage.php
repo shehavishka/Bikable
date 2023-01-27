2a094331c685fdd81b2506de916ec915670acd76
shehavishka <2020cs018@stu.ucsc.cmb.ac.lk>
2023-01-27 13:06:10 +0530

    tried to get image from the database but found a bug

--- app/views/owners/ownerLandPage.php
+++ app/views/owners/ownerLandPage.php
@@ -28,9 +28,10 @@
         </div>
 
         <div class="dashboard__user__detail">
-            <div class="user__address">Hello, Shehaan</div>
+            <div class="user__address">Hello, <?php echo $_SESSION['user_fName'];?></div>
             <img src="<?php echo URLROOT;?>/public/images/owners/dashboardIcons/avatar.png" alt="dashboard profile picture" class="imgProperty">
-        </div>      
+            <!-- <img src="data:image/jpeg;base64,<?php echo $_SESSION['user_picture']; ?>" alt="dashboard profile picture" class="imgProperty"> -->
+        </div>
     </section>
 
 
