ad2e17df37e4df9edefa87b2f3541e6e8a87dad6
shehavishka <2020cs018@stu.ucsc.cmb.ac.lk>
2023-01-31 09:20:49 +0530

    implemented findNicNumber feature in Owner database file

--- app/models/Owner.php
+++ app/models/Owner.php
@@ -12,7 +12,7 @@
         */
 
         public function findUserByEmail($userEmail){
-            $this->db->prepareQuery("SELECT * FROM users where email = '$userEmail'");
+            $this->db->prepareQuery("SELECT * FROM users where emailAdd = '$userEmail'");
             // $this->db->bind(':email', $userEmail);
 
             $this->db->single();
@@ -25,6 +25,18 @@
             }
         }
 
+        public function findNicNumber($userNIC){
+            $this->db->prepareQuery("SELECT * FROM users where NIC = '$userNIC'");
+
+            $this->db->single();
+
+            //check row
+            if($this->db->rowCount() > 0){
+                return true;
+            }else{
+                return false;
+            }
+        }
 
 
     }
