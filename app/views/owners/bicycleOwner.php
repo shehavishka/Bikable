5106ac56dbd6ca7b4453ac077d3c97284de46320
shehavishka <2020cs018@stu.ucsc.cmb.ac.lk>
2023-02-03 17:42:59 +0530

    removed unwanted dummy files

--- app/views/owners/bicycleOwner.php
+++ app/views/owners/bicycleOwner.php
@@ -48,103 +48,52 @@
 
                 </tr>
 
-                <tr>
+                <!-- <tr>
                     <td><input type="checkbox" class="cbox"></td>
                     <td>Shehaan Avishka</td>
                     <td>1238524</td>
                     <td>Inactive</td>
                     <td>OwnerShehaan@gmail.com</td>
                     <td>200002403065</td>
-                    <td>B.Owner</td>
+                    <td>Admin</td>
                     <td>
-                        <!-- update icon svg format -->
+                        update icon svg format
                         <svg width="34" height="34" viewBox="0 0 34 34" fill="none" xmlns="http://www.w3.org/2000/svg">
                             <circle cx="17" cy="17" r="17" fill="black"/>
                             <path d="M19.06 14L20 14.94L10.92 24H10V23.08L19.06 14ZM22.66 8C22.41 8 22.15 8.1 21.96 8.29L20.13 10.12L23.88 13.87L25.71 12.04C26.1 11.65 26.1 11 25.71 10.63L23.37 8.29C23.17 8.09 22.92 8 22.66 8ZM19.06 11.19L8 22.25V26H11.75L22.81 14.94L19.06 11.19Z" fill="white"/>
                         </svg>
                             
                     </td>
-                </tr>
-                
-                <tr>
-                    <td><input type="checkbox"></td>
-                    <td>Shehaan Avishka</td>
-                    <td>1238524</td>
-                    <td>Inactive</td>
-                    <td>OwnerShehaan@gmail.com</td>
-                    <td>200002403065</td>
-                    <td>B.Owner</td>
-                    <td>
-                        <!-- update icon svg format -->
-                        <svg width="34" height="34" viewBox="0 0 34 34" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <circle cx="17" cy="17" r="17" fill="black"/>
-                            <path d="M19.06 14L20 14.94L10.92 24H10V23.08L19.06 14ZM22.66 8C22.41 8 22.15 8.1 21.96 8.29L20.13 10.12L23.88 13.87L25.71 12.04C26.1 11.65 26.1 11 25.71 10.63L23.37 8.29C23.17 8.09 22.92 8 22.66 8ZM19.06 11.19L8 22.25V26H11.75L22.81 14.94L19.06 11.19Z" fill="white"/>
-                        </svg>
-                            
-                    </td>
-                </tr>
+                </tr> -->
+        
 
-
-
-                <tr>
-                    <td><input type="checkbox"></td>
-                    <td>Shehaan Avishka</td>
-                    <td>1238524</td>
-                    <td>Inactive</td>
-                    <td>OwnerShehaan@gmail.com</td>
-                    <td>200002403065</td>
-                    <td>B.Owner</td>
-                    <td>
-                        <!-- update icon svg format -->
-                        <svg width="34" height="34" viewBox="0 0 34 34" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <circle cx="17" cy="17" r="17" fill="black"/>
-                            <path d="M19.06 14L20 14.94L10.92 24H10V23.08L19.06 14ZM22.66 8C22.41 8 22.15 8.1 21.96 8.29L20.13 10.12L23.88 13.87L25.71 12.04C26.1 11.65 26.1 11 25.71 10.63L23.37 8.29C23.17 8.09 22.92 8 22.66 8ZM19.06 11.19L8 22.25V26H11.75L22.81 14.94L19.06 11.19Z" fill="white"/>
-                        </svg>
-                            
-                    </td>
-                </tr>
-
-
-                <tr>
-                    <td><input type="checkbox"></td>
-                    <td>Shehaan Avishka</td>
-                    <td>1238524</td>
-                    <td>Inactive</td>
-                    <td>OwnerShehaan@gmail.com</td>
-                    <td>200002403065</td>
-                    <td>B.Owner</td>
-                    <td>
-                        <!-- update icon svg format -->
-                        <svg width="34" height="34" viewBox="0 0 34 34" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <circle cx="17" cy="17" r="17" fill="black"/>
-                            <path d="M19.06 14L20 14.94L10.92 24H10V23.08L19.06 14ZM22.66 8C22.41 8 22.15 8.1 21.96 8.29L20.13 10.12L23.88 13.87L25.71 12.04C26.1 11.65 26.1 11 25.71 10.63L23.37 8.29C23.17 8.09 22.92 8 22.66 8ZM19.06 11.19L8 22.25V26H11.75L22.81 14.94L19.06 11.19Z" fill="white"/>
-                        </svg>
-                            
-                    </td>
-                </tr>
-
-
-
-
-                <!-- <?php foreach($data['admin_details'] as $oneAdmin) : ?>
+                <?php foreach($data['bikeOwner_details'] as $oneAdmin) : ?>
                     <tr>
-                        <td><?php echo $oneAdmin->first_name ?></td>
-                        <td><?php echo $oneAdmin->last_name ?></td>
-                        <td><?php echo $oneAdmin->email ?></td>
-                        <td><?php echo $oneAdmin->nic ?></td>
+                        <td><input type="checkbox"></td>
+                        <td><?php echo $oneAdmin->firstName . " " . $oneAdmin->lastName ?></td>
+                        <td><?php echo $oneAdmin->userID ?></td>
                         <td>
                             <?php 
-                                if($oneAdmin->status == "1"){
-                                    echo "Inactive";
+                                if($oneAdmin->status == 1){
+                                    echo "Active";
                                 }else{
-                                    echo "Deactive";
+                                    echo "Inactive";
                                 }
                             
                             ?>
                         </td>
+                        <td><?php echo $oneAdmin->emailAdd ?></td>
+                        <td><?php echo $oneAdmin->NIC ?></td>
                         <td><?php echo $oneAdmin->role ?></td>
+                        <td>
+                        <!-- update icon svg format -->
+                        <svg width="34" height="34" viewBox="0 0 34 34" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <circle cx="17" cy="17" r="17" fill="black"/>
+                            <path d="M19.06 14L20 14.94L10.92 24H10V23.08L19.06 14ZM22.66 8C22.41 8 22.15 8.1 21.96 8.29L20.13 10.12L23.88 13.87L25.71 12.04C26.1 11.65 26.1 11 25.71 10.63L23.37 8.29C23.17 8.09 22.92 8 22.66 8ZM19.06 11.19L8 22.25V26H11.75L22.81 14.94L19.06 11.19Z" fill="white"/>
+                        </svg>
+                            
                     </tr>
-                <?php endforeach; ?> -->
+                <?php endforeach; ?>
 
             </table>
         </div>
