c90556d3902905f68ab40a62ce7d469176b98846
shehavishka <2020cs018@stu.ucsc.cmb.ac.lk>
2023-02-06 22:03:54 +0530

    administrator css file set up to decorate edit profile button

--- app/views/owners/ownerLandPage.php
+++ app/views/owners/ownerLandPage.php
@@ -55,133 +55,139 @@
         </div>
 
         <!-- report card on the upper section of the display -->
-        <div class="upper__section--reports cardd">
-            <div class="upper__section__card--title">
-                Reports
-            </div>
-            <div class="upper_section__reports--body">
-                <!-- take reports data from the database and display on this table -->
-                <table>
-                    <tr>
-                        <th>Report ID</th>
-                        <th>Mechanic ID</th>
-                    </tr>
-                    <tr>
-                        <td>2902012</td>
-                        <td>200002403065</td>
-                    </tr>
-                    <tr>
-                        <td>2902012</td>
-                        <td>200002403065</td>
-                    </tr>
-                    <tr>
-                        <td>2902012</td>
-                        <td>200002403065</td>
-                    </tr>
-                    <tr>
-                        <td>2902012</td>
-                        <td>200002403065</td>
-                    </tr>
-                    <tr>
-                        <td>2902012</td>
-                        <td>200002403065</td>
-                    </tr>
-                </table>
-            </div>
-        </div>
+            <a href="<?php echo URLROOT ?>/owners/reportsControl" style="text-decoration:none;color:black" class="anchor--card">
+                <div class="upper__section--reports cardd">
+                        <div class="upper__section__card--title" style="text-decoration:none">
+                            Reports
+                        </div>
+                    <div class="upper_section__reports--body">
+                        <!-- take reports data from the database and display on this table -->
+                        <table>
+                            <tr>
+                                <th>Report ID</th>
+                                <th>Mechanic ID</th>
+                            </tr>
+                            <tr>
+                                <td>2902012</td>
+                                <td>200002403065</td>
+                            </tr>
+                            <tr>
+                                <td>2902012</td>
+                                <td>200002403065</td>
+                            </tr>
+                            <tr>
+                                <td>2902012</td>
+                                <td>200002403065</td>
+                            </tr>
+                            <tr>
+                                <td>2902012</td>
+                                <td>200002403065</td>
+                            </tr>
+                            <tr>
+                                <td>2902012</td>
+                                <td>200002403065</td>
+                            </tr>
+                        </table>
+                    </div>
+                </div>
+            </a>
 
         <!-- upper section reapir log card -->
-        <div class="upper__section--repairlog cardd">
-            <div class="upper__section__card--title">
-                Repair Log(Active)
+        <a href="<?php echo URLROOT ?>/owners/addNewRepairLog" style="text-decoration:none;color:black">
+            <div class="upper__section--repairlog cardd">
+                <div class="upper__section__card--title">
+                    Repair Log(Active)
+                </div>
+
+                <div class="upper__section__repairlog--body">
+                    <table>
+                        <tr>
+                            <th>Log ID</th>
+                            <th>Bicyle ID</th>
+                            <th>Date in</th>
+                        </tr>
+
+                        <tr>
+                            <td>M32465</td>
+                            <td>34156D</td>
+                            <td>2022.11.11</td>
+                        </tr>
+
+                        <tr>
+                            <td>F35325</td>
+                            <td>26134F</td>
+                            <td>2022.11.11</td>
+                        </tr>
+
+                        <tr>
+                            <td>J31535</td>
+                            <td>34524G</td>
+                            <td>2022.11.11</td>
+                        </tr>
+
+                        <tr>
+                            <td>L78975</td>
+                            <td>25236D</td>
+                            <td>2022.11.11</td>
+                        </tr>
+
+                        <tr>
+                            <td>F35325</td>
+                            <td>26134F</td>
+                            <td>2022.11.11</td>
+                        </tr>
+                    </table>
+                </div>
             </div>
+        </a>
 
-            <div class="upper__section__repairlog--body">
-                <table>
-                    <tr>
-                        <th>Log ID</th>
-                        <th>Bicyle ID</th>
-                        <th>Date in</th>
-                    </tr>
+        <a href="<?php echo URLROOT ?>/owners/bicyclesControl" style="text-decoration:none;color:black">
+            <div class="upper__section--bicycles cardd">
+                <div class="upper__section__card--title">
+                    Bicycles
+                </div>
+                <div class="upper__section__bicycles--body">
+                    <table>
+                        <tr>
+                            <th>Bicycle ID</th>
+                            <th>Frame</th>
+                            <th>Status</th>
+                        </tr>
 
-                    <tr>
-                        <td>M32465</td>
-                        <td>34156D</td>
-                        <td>2022.11.11</td>
-                    </tr>
+                        <tr>
+                            <td>34156D</td>
+                            <td>L</td>
+                            <td>Active</td>
+                        </tr>
 
-                    <tr>
-                        <td>F35325</td>
-                        <td>26134F</td>
-                        <td>2022.11.11</td>
-                    </tr>
+                        <tr>
+                            <td>34152D</td>
+                            <td>L</td>
+                            <td>Active</td>
+                        </tr>
 
-                    <tr>
-                        <td>J31535</td>
-                        <td>34524G</td>
-                        <td>2022.11.11</td>
-                    </tr>
+                        <tr>
+                            <td>43214Q</td>
+                            <td>S</td>
+                            <td>Active</td>
+                        </tr>
 
-                    <tr>
-                        <td>L78975</td>
-                        <td>25236D</td>
-                        <td>2022.11.11</td>
-                    </tr>
+                        <tr>
+                            <td>43214Q</td>
+                            <td>S</td>
+                            <td>Active</td>
+                        </tr>
 
-                    <tr>
-                        <td>F35325</td>
-                        <td>26134F</td>
-                        <td>2022.11.11</td>
-                    </tr>
-                </table>
+                        <tr>
+                            <td>43214Q</td>
+                            <td>M</td>
+                            <td>Inactive</td>
+                        </tr>
+
+                    </table>
+                </div>
             </div>
-        </div>
-
-        <div class="upper__section--bicycles cardd">
-            <div class="upper__section__card--title">
-                Bicycles
-            </div>
-            <div class="upper__section__bicycles--body">
-                <table>
-                    <tr>
-                        <th>Bicycle ID</th>
-                        <th>Frame</th>
-                        <th>Status</th>
-                    </tr>
-
-                    <tr>
-                        <td>34156D</td>
-                        <td>L</td>
-                        <td>Active</td>
-                    </tr>
-
-                    <tr>
-                        <td>34152D</td>
-                        <td>L</td>
-                        <td>Active</td>
-                    </tr>
-
-                    <tr>
-                        <td>43214Q</td>
-                        <td>S</td>
-                        <td>Active</td>
-                    </tr>
-
-                    <tr>
-                        <td>43214Q</td>
-                        <td>S</td>
-                        <td>Active</td>
-                    </tr>
-
-                    <tr>
-                        <td>43214Q</td>
-                        <td>M</td>
-                        <td>Inactive</td>
-                    </tr>
-
-                </table>
-            </div>
-        </div>
+        </a>
     </section>
 
     <!-- ***************************************************************************************************************** -->
