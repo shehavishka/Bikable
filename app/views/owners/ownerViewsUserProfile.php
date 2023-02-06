c6a40cc2d96a9a91bc1c771a42cd2f27c9bec2cf
shehavishka <2020cs018@stu.ucsc.cmb.ac.lk>
2023-02-06 22:00:49 +0530

    if we want to suspend user before to do it need to view his profile and to do that created user profile into the system

---
+++ app/views/owners/ownerViewsUserProfile.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="<?php echo URLROOT; ?>/public/css/owners/ownerViewsUserProfile.css">
+    <title>User Profile</title>
+</head>
+<body>
+    <section class="data__area">
+        <div class="left--side">
+            <div class="profile__picture--card">
+                <!-- <div class="dropdown_area">
+                    <?php echo '<img src="data:image/jpeg;base64,'.base64_encode($_SESSION['user_picture']).'" alt="dashboard profile picture" class="imgProperty"'; ?>
+                </div>                 -->
+
+                <div class="image_part">
+                    <img src="<?php echo URLROOT;?>/public/images/z_bikableLogo/logo.PNG" alt="">
+                </div>
+                <div class="user_history">
+                    <table>
+                        <tr>
+                            <th style="width: 3%;"></th>
+                            <th style="width: 5%;"></th>
+        
+                        </tr>
+                        <tr>
+                            <td>User ID</td>
+                            <td><?php echo $data['userDetailObject']->userID; ?></td>
+                        </tr>
+                        <tr>
+                            <td>Last Logged in  :</td>
+                            <td>2023/02/5</td>
+                        </tr>
+                        <tr>
+                            <td>Registered date : </td>
+                            <td>2022/12/28</td>
+                        </tr>
+                    </table>
+                </div>
+
+            </div>
+
+        </div>
+        <div class="right--side">
+            <div class="detail__view--card">
+                <div class="generalInformation"><strong><h2>General Information</h2></strong></div>
+                <div class="user__detail">
+                    <table>
+                        <tr>
+                            <td>Name</td>
+                            <td><?php echo $data['userDetailObject']->firstName . ' ' . $data['userDetailObject']->lastName; ?></td>
+                        </tr>
+                        <tr>
+                            <td>NIC</td>
+                            <td><?php echo $data['userDetailObject']->NIC; ?></td>
+                        </tr>
+                        <tr>
+                            <td>Role</td>
+                            <td><?php echo $data['userDetailObject']->role; ?></td>
+                        </tr>
+                        <tr>
+                            <td>Mobile Number</td>
+                            <td><?php echo $data['userDetailObject']->phoneNumber; ?></td>
+                        </tr>
+                        <tr>
+                            <td>Email</td>
+                            <td><?php echo $data['userDetailObject']->emailAdd; ?></td>
+                        </tr>
+                        <tr>
+                            <td>Status</td>
+                            <td>
+                                <?php
+                                    if($data['userDetailObject']->status == 1){
+                                        echo "Active";
+                                    }else{
+                                        echo "Inactive";
+                                    }
+                                ?>
+                            </td>
+                        </tr>
+
+                    </table>
+                </div>
+            </div>
+            <div class="button__area">
+                <input type="button" value="Suspend" class="btn">
+            </div>
+        </div>
+    </section>
+
+
+</body>
+</html>
