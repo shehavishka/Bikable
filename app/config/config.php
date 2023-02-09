3aa9d3054088f3573d07568428723f909fd01215
shehavishka <2020cs018@stu.ucsc.cmb.ac.lk>
2023-02-09 13:34:14 +0530

    config.php file changed

--- app/config/config.php
+++ app/config/config.php
@@ -7,7 +7,7 @@
     define('DB_HOST', 'localhost');
     define('DB_USER', 'root');
     define('DB_PASS', '');
-    define('DB_NAME', 'Bikable');
+    define('DB_NAME', 'bikable');
 
     define('URLROOT', 'http://localhost/Bikable_23.1.1');
 
