24610a97c535263408d6ba7ea377b60f4c6d71fb
Anjali <anjalikrodrigo@gmail.com>
2023-02-16 13:31:01 +0530

    inital code for the error page thing - need to figure out the css and add it to everywhere it says die

---
+++ app/views/admins/error404.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="<?php echo URLROOT; ?>/public/css/admins/error404.css">
+    <title>Add Bicycle</title>
+</head>
+<body>
+    <section class = "main">
+        <h1>Internal Server Error</h1>
+        <image src="<?php echo URLROOT; ?>/public/images/inc/error404.png">
+        <h2>Sorry, something went wrong</h2>
+        <p>Try again later</p>
+        <a href="<?php echo URLROOT; ?>/admins/adminLandPage">Back to Home</a>
+    </section>
+
+</body>
+</html>
