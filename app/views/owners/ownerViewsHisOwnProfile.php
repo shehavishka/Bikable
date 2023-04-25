df01e74e9232accdd73428091265df508af0d8f8
shehavishka <2020cs018@stu.ucsc.cmb.ac.lk>
2023-04-25 14:58:30 +0530

    ownerViewsHisOwnProfile linked with onwerViewsHisOwnProfile css file

--- app/views/owners/ownerViewsHisOwnProfile.php
+++ app/views/owners/ownerViewsHisOwnProfile.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="ownerViewHisOwnProfile.css">
+    <link rel="stylesheet" href="<?php echo URLROOT; ?>/public/css/owners/ownerViewsHisOwnProfile.css>
     <title>Owner Profile</title>
 </head>
 <body>
