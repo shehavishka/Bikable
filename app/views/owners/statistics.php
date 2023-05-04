b3abf7f6a0d03a7e8d757306fc5b8af5982709c6
shehavishka <2020cs018@stu.ucsc.cmb.ac.lk>
2023-05-04 10:26:16 +0530

    designed statistics page

--- app/views/owners/statistics.php
+++ app/views/owners/statistics.php
@@ -5,6 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="<?php echo URLROOT; ?>/public/css/owners/statistics.css">
+    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
     <title>Statistics</title>
 </head>
 <body>
@@ -64,7 +65,7 @@
 
 
         <div class="data__count--area">
-            <div class="data__count__area--totalRiders">
+            <div class="data__count__area--totalRiders cardd">
                 <div class="data__count__area--totalRiders--value">
                     100
                 </div>
@@ -72,7 +73,7 @@
                     Total Riders
                 </div>
             </div>
-            <div class="data__count__area--totalBikes">
+            <div class="data__count__area--totalBikes cardd">
                 <div class="data__count__area--totalBikes--value">
                     200
                 </div>
@@ -80,7 +81,7 @@
                     Total Bikes
                 </div>
             </div>
-            <div class="data__count__area--totalDockingAreas">
+            <div class="data__count__area--totalDockingAreas cardd">
                 <div class="data__count__area--totalDockingAreas--value">
                     10
                 </div>
@@ -88,7 +89,7 @@
                     Total Docking Areas
                 </div>
             </div>
-            <div class="data__count__area--activeReports">
+            <div class="data__count__area--activeReports cardd">
                 <div class="data__count__area--activeReports--value">
                     13
                 </div>
@@ -98,25 +99,156 @@
             </div>
         </div>
 
-        <section class="graphs-Area">
-            <div class="top_left-graph">
-                <h1>Graph 1</h1>
+        <section class="graphs-Area1">
+            <div class="top_left-g">
+                <div class="lower__section__card--title" styles="margin-bottom: -10px;">
+                    Bike Inventory Status
+                </div>
+                <!-- DOUGHNUT CHART -->
+                <div class="lower__section__card--doughnuts">
+                    <canvas id="myChart" width="20" height="20"></canvas>
+                </div>
+                <script>
+                        const data1 = {
+                            labels: [
+                                'Occupied Bikes',
+                                'Vacant Bikes',
+                                'Bikes in Repair'
+                            ],
+                            datasets: [{
+                                label: 'Bikes',
+                                data: [300, 50, 100],
+                                backgroundColor: [
+                                    'rgb(28, 28, 28)',
+                                    'rgb(158, 158, 157)',
+                                    'rgb(222, 222, 222)'
+                                ],
+                                hoverOffset: 4
+                            }]
+                        };
+
+                        const config = {
+                            type: 'doughnut',
+                            data: data1,
+                            options: {
+                                cutout: '70%',
+                                fontSize: 12,
+                                plugins: {
+                                    legend: {
+                                        display: true,
+                                        position: 'top',
+                                        align: 'center',
+                                        labels: {
+                                            font: {
+                                                family: "'Helvetica Neue', 'Helvetica', 'Arial', sans-serif",
+                                                size: 10,
+                                            },
+                                            padding: 10,
+                                            boxWidth: 80,
+                                            usePointStyle: true
+                                        }
+                                    },
+                                    // datalabels: {
+                                    //     color: '#fff',
+                                    //     font: {
+                                    //         size: 12,
+                                    //     },
+                                    //     formatter: (value, context) => {
+                                    //         const total = context.chart.data.datasets[0].data.reduce((acc, val) => acc + val, 0);
+                                    //         const percentage = Math.round((value / total) * 100);
+                                    //         return `${value} (${percentage}%)`;
+                                    //     },
+                                    // },
+                                    
+                                },
+                                // scales: {
+                                //     y: {
+                                //         beginAtZero: true,
+                                //     },
+                                // },
+                            },
+                        };
+                        var myChart = new Chart(
+                            document.getElementById('myChart'),
+                            config
+                        );
+                </script>
             </div>
-            <div class="top_middle-graph">
-                <h1>Graph 2</h1>
+
+            <div class="top_righ-g">
+                <div class="lower__section__card--title" style="margin-top: 5px; margin-left: 7%">
+                    Bike Availability Trend
+                </div>
+                <div class="lower__section__card--bars" style="margin-top: 5px;">
+                    <canvas id="myLine" width="50" height="20"></canvas>
+                </div>  
+                <script>
+                    const data2 = {
+                            labels: [
+                                "Mon","Tue","Wed","Thu","Fri","Sat","Sun"
+                            ],
+                            datasets: [{
+                                    label: "Vacant Bikes",
+                                    data: [180,200,150,120,100,150,200],
+                                    borderColor: "rgba(255, 99, 132, 1)",
+                                    backgroundColor: "rgba(255, 99, 132, 0.2)",
+                                    fill: true,
+                                    tension: 0.1,
+                                },
+                                {
+                                    label: "Occupied Bikes",
+                                    data: [100,80,120,150,200,150,100],
+                                    borderColor: "rgba(54, 162, 235, 1)",
+                                    backgroundColor: "rgba(54, 162, 235, 0.2)",
+                                    fill: true,
+                                    tension: 0.4,
+                                },
+                            ]
+                    };
+
+                        const config2 = {
+                            type: 'line',
+                            data: data2,
+                            options: {
+                                plugins: {
+                                    legend: {
+                                        position: "bottom",
+                                    },
+                                },
+                                scales: {
+                                    x: {
+                                        display: true,
+                                        title: {
+                                            display: true,
+                                            text: "Days",
+                                        },
+                                    },
+                                    y: {
+                                        display: true,
+                                        title: {
+                                            display: true,
+                                            text: "Number of Bikes",
+                                        },
+                                        suggestedMin: 50,
+                                    },
+                                },
+                            },
+                        };
+                        var myChart = new Chart(
+                            document.getElementById('myLine'),
+                            config2
+                    );
+                </script>
             </div>
-            <div class="top_right-graph">
-                <h1>Graph 3</h1>
-            </div>
-            <div class="bottom_left-graph">
+            <!-- <div class="bottom_left-g">
                 <h1>Graph 4</h1>
             </div>
-            <div class="bottom_middle-graph">
+            <div class="bottom_middle-g">
                 <h1>Graph 5</h1>
             </div>
-            <div class="bottom_right-graph">
+            <div class="bottom_right-g">
                 <h1>Graph 6</h1>
-            </div>
+            </div> -->
         </section>
     </section>
 
