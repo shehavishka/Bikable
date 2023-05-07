db266b40994bb0e34981050a968a19f6bcd996c7
shehavishka <2020cs018@stu.ucsc.cmb.ac.lk>
2023-05-07 19:41:15 +0530

    reports delete selected button setup

--- app/views/owners/reports.php
+++ app/views/owners/reports.php
@@ -27,8 +27,9 @@
                 <div class="add_user_button">
                     <input type="button" class="btn btn_add" value="View Archieved" onclick="location.href='<?php echo URLROOT;?>/owners/archivedReportsControl'">
                 </div>
+    <form action="<?php echo URLROOT;?>/owners/archiveReports" method="POST" id="userInterface">
                 <div class="delete_user_button">
-                    <input type="button" class="btn btn_delete" value="Archive Selected" onclick="location.href='<?php echo URLROOT;?>/owners/addAdministrator'">
+                    <input type="submit" class="btn btn_delete" value="Archive Selected">
                 </div>
             </div>
 
@@ -73,7 +74,7 @@
 
                 <?php foreach($data['reports_details'] as $oneReport) : ?>
                     <tr>
-                        <td><input type="checkbox" class="cbox"></td>
+                        <td><input type="checkbox" name="selected[]" value="<?php echo $oneReport->reportID ?>"></td>
                         <td><?php echo $oneReport->reportID ?></td>
                         <td><?php echo $oneReport->reporterID ?></td>
                         <td><?php echo $oneReport->status ?></td>
@@ -82,19 +83,42 @@
                         <td><?php echo $oneReport->loggedTimestamp ?></td>
                         <td><?php echo $oneReport->reportType ?></td>
                         <td>
-                        <!-- update icon svg format -->
-                        <svg width="34" height="34" viewBox="0 0 34 34" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <circle cx="17" cy="17" r="17" fill="black"/>
-                            <path d="M19.06 14L20 14.94L10.92 24H10V23.08L19.06 14ZM22.66 8C22.41 8 22.15 8.1 21.96 8.29L20.13 10.12L23.88 13.87L25.71 12.04C26.1 11.65 26.1 11 25.71 10.63L23.37 8.29C23.17 8.09 22.92 8 22.66 8ZM19.06 11.19L8 22.25V26H11.75L22.81 14.94L19.06 11.19Z" fill="white"/>
-                        </svg>
-                            
+                            <!-- <a href="<?php echo URLROOT;?>/admins/editReportDetails?reportID=<?php echo $oneObject->reportID;?>"><img src="<?php echo URLROOT;?>/public/images/admins/editIcon1.png" alt="edit"></a> -->
                         </td>
                     </tr>
                 <?php endforeach; ?>
 
             </table>
+    </form>
         </div>
     </section>
+
+    <script>
+        document.querySelector('form').addEventListener('submit', function(event) {
+            event.preventDefault(); // prevent the form from submitting
+
+            // get all the checkboxes in the table
+            const checkboxes = document.querySelectorAll('table input[type="checkbox"]');
+
+            // collect the values of the checked checkboxes
+            const selectedRows = [];
+            checkboxes.forEach(function(checkbox) {
+            if (checkbox.checked) {
+                selectedRows.push(checkbox.value);
+            }
+            });
+
+            // add the selected rows to a hidden input field in the form
+            const input = document.createElement('input');
+            input.setAttribute('type', 'hidden');
+            input.setAttribute('name', 'selectedRows');
+            input.setAttribute('value', JSON.stringify(selectedRows));
+            this.appendChild(input);
+
+            // submit the form
+            this.submit();
+        });
+    </script>
 
 
 
