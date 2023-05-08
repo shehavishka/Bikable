69a61cf0cc5c012b540eec601083aecb6077c7f9
shehavishka <2020cs018@stu.ucsc.cmb.ac.lk>
2023-05-08 10:44:20 +0530

    mechanic search view setup

--- app/views/owners/mechanic.php
+++ app/views/owners/mechanic.php
@@ -111,6 +111,34 @@
         </div>
     </section>
 
+    <script>
+        $(document).ready(function(){
+            $("#search").keyup(function(){
+                var searchText = $(this).val();
+
+                if(searchText)
+                {
+                    $('.all-data').hide();
+                    $('.search-data').show();
+                }
+                else{
+                    $('.all-data').show();
+                    $('.search-data').hide();
+                }
+
+                $.ajax({
+                    url: './search_mechanics',
+                    type: 'POST',
+                    data: {search: searchText},
+                    success: function(response){
+                        console.log(response);
+                        $("#details").html(response);
+                    }
+                });
+            });       
+        });
+    </script>
+
 
 
 </body>
